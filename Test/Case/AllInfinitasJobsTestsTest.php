94fb83e6e10a9c0a1c948aa7df1e066b0ad11d20
dogmatic69 <dogmatic69@gmail.com>
2012-09-29 23:46:02 +0100

    adding missing group test

---
+++ Test/Case/AllInfinitasJobsTestsTest.php
@@ -0,0 +1,17 @@
+<?php
+class AllInfinitasJobsTestsTest extends PHPUnit_Framework_TestSuite {
+
+/**
+ * Suite define the tests for this suite
+ *
+ * @return void
+ */
+	public static function suite() {
+		$suite = new CakeTestSuite('All InfinitasJobs test');
+
+		$path = CakePlugin::path('InfinitasJobs') . 'Test' . DS . 'Case' . DS;
+		$suite->addTestDirectoryRecursive($path);
+
+		return $suite;
+	}
+}
